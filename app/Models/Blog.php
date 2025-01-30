e729890b1054b9c2a9bd5534ded893b2ef617e2a
Md Arif Hossen <arifsohag2500@gmail.com>
2025-01-30 23:33:15 +0600

    observer

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+
+#[ObservedBy([BlogObserver::class])]
 class Blog extends Model
 {
     use HasFactory;
